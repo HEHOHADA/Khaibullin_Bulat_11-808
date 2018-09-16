6cbed65b93c0317d1b07888eb9d8928a4107ee3e
Bulat <bulat5512@yandex.ru>
2018-09-16 21:11:20 +0300

    change wrong file expr8

--- 2018/fall/sem/The first with C/EXPR8.cs
+++ 2018/fall/sem/The first with C/EXPR8.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EXPR8
+namespace Expr8
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Введите x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите а");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            //find free number of second line
+            double b2 = y + (x / a);
+            //find point of intersection
+            double x1 = ((b2 - b) * a) / (a * a + 1);
+            double y1 = (a * x1) + b;
+            Console.WriteLine("x = "+x1+" y = "+y1);
+            Console.ReadKey();
         }
     }
 }
