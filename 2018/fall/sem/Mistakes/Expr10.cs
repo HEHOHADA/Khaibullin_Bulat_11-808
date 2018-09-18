f911861583ac6b719cd0aba026473f6f5e713ee4
Bulat <bulat5512@yandex.ru>
2018-09-18 14:41:28 +0300

    new task 10 and 11

---
+++ 2018/fall/sem/Mistakes/Expr10.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace expr10._1
+{
+    class Program
+    { // Khaibullin Bulat (find sum <1000 /3 abd /5)
+        static void Main(string[] args)
+        {
+            //find sum of number /3 + sum /5 and minus sum number /15
+            var sumKr3 = (3 + 999) / 2 * 333;
+            var sumKr5 = (5 + 995) / 2 * 199;
+            var sumKr15 = (15 + 990) * 33;
+            var endSum = sumKr3 + sumKr5 - sumKr15;
+            Console.WriteLine(endSum);
+            Console.ReadKey();
+        }
+    }    
+}
---
+++ 2018/fall/sem/Mistakes/expr11.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expr11._1
+{
+    class Program
+    {// Khaibullin Bulat ( programm find angle between hour and minute )
+        static void Main(string[] args)
+        {
+            int angleHour = 30;
+            int angleMinute = 6;
+            var speedMinute = 1 / 2;
+            Console.WriteLine("Ввод часов");
+            var timeHour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ввод минут");
+            var timeMinute = Convert.ToInt32(Console.ReadLine());
+            var endAngle = Math.Abs((timeHour * angleHour + speedMinute * timeMinute) - angleMinute * timeMinute);
+            if (endAngle >180)
+            {
+                endAngle = endAngle - 180;
+            }
+            Console.WriteLine(endAngle);
+            Console.ReadKey();
+
+        }
+    }
+}
