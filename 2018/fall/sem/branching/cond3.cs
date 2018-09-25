9b71056eafd2391ad6ea62ff534dfb1b5b71afa3
Bulat <bulat5512@yandex.ru>
2018-09-25 20:47:43 +0300

    cond3

---
+++ 2018/fall/sem/branching/cond3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace COND3
+{
+    class Program
+    {   //find number sum
+        public static int Sum(int numb,int sum)
+        {
+            for (int i = 0; i<3; i++)
+            {
+                sum += numb % 10;
+                numb = numb / 10;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            var number = Convert.ToInt32(Console.ReadLine());
+            // lucky sum-1 and sum+1
+            if ( Sum(number / 1000, 0) == Sum((number % 1000)-1, 0)  || Sum(number / 1000, 0) == Sum((number % 1000)+1, 0) )
+                Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+            Console.ReadKey();
+
+        }
+    }
+}
