d2413ddfa8e7cb50064ec839f98f801794c1d370
Bulat <bulat5512@yandex.ru>
2018-09-25 20:45:39 +0300

    cond1,2

---
+++ 2018/fall/sem/branching/COnd1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cond1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// делим строку и  находим перемещение по вертикали и горизонтали
+            string[] input = Console.ReadLine().Split();
+            var dx = Math.Abs(Convert.ToInt32(input[1][0]) - Convert.ToInt32(input[0][0]));
+            var dy = Math.Abs(Convert.ToInt32(input[1][1]) - Convert.ToInt32(input[0][1]));
+            //рассматриваем для хода, все фигуры(кроме пешки)
+            if (dx == dy && dy != 0 && dx != 0 || dx == 0 && dy != 0 || dy == 0 && dx != 0)
+                Console.WriteLine("Для Ферзя");
+            if ((dx<=1 &&dy<=1)&&(dx == dy && dy != 0 && dx != 0 || dx == 0 && dy != 0 || dy == 0 && dx != 0))
+                Console.WriteLine("Для короля");
+            if (dx==dy&& dx!=0)
+                Console.WriteLine("Для слона");
+            if (dx==2 && dy==1|| dx==1 && dy==2)
+                Console.WriteLine("Для коня");
+            if (dx!= 0 && dy==0 || dy!=0 && dx==0)
+                Console.WriteLine("Для ладьи");
+            else Console.WriteLine("не двигается");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2018/fall/sem/branching/Cond2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cond2
+{
+    class Program
+    {
+        public static bool Compare(double side, int a, int b)
+        {
+            return side <= Math.Max(a, b);
+        }
+
+        static void Main(string[] args)
+        {
+            var x = Convert.ToInt32(Console.Read());
+            var y = Convert.ToInt32(Console.Read());
+            var z = Convert.ToInt32(Console.Read());
+            var a = Convert.ToInt32(Console.Read());
+            var b = Convert.ToInt32(Console.Read());
+            int n = 0;
+            if (Compare(x,a,b)) n++;
+            if (Compare(y,a,b)) n++;
+            if (Compare(z,a,b)) n++;
+            if (n >= 2) Console.WriteLine("Влезет");
+            else Console.WriteLine("Не влезет");
+            Console.ReadKey();
+        }
+    }
+}
