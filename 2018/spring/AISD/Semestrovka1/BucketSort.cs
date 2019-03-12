838e80c9b42ee611fbea86155494165706bf6a81
Bulat <bulat5512@yandex.ru>
2019-03-12 13:08:06 +0300

    added Bucket Sort

---
+++ 2018/spring/AISD/Semestrovka1/BucketSort.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace bucketlist
+{
+    class Program
+    {
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            var arrayGraph = new Series();
+            var linkedGraph = new Series();
+            string[] input;
+            Generate.Generete();
+            for (int i = 1; i <= 100; i++)
+            {
+                GC.Collect();
+                input = File.ReadAllLines(string.Format("data{0}.txt", i));
+                int[] array = new int[input.Length];
+                for (int j = 0; j < input.Length; j++)
+                    array[j] = Convert.ToInt32(input[j]);
+                // поо два Графика, запускал отдельно
+               //DrawGraph.MeasureTime(array, BucketSort.BucketSortWithArray, arrayGraph);
+                //DrawGraph.MeasureTime(array, BucketSortLinked.BucketSortWithLinked, linkedGraph);
+                BucketSort.MeasureTimeIntegration(array, BucketSort.BucketSortWithArray, arrayGraph);
+                BucketSortLinked.MeasureTimeIntegration(array, BucketSortLinked.BucketSortWithLinked, linkedGraph);
+            }
+            var chart = DrawGraph.MakeChart(arrayGraph, linkedGraph);
+            var form = new Form();
+            form.ClientSize = new Size(800, 600);
+            form.Controls.Add(chart);
+            Application.Run(form);
+        }        
+    }
+
+    public class BucketSort
+    {
+        static int interationsArray = 0;
+
+        public static int[] BucketSortWithArray(int[] unsorted)
+        {
+            var max = unsorted.Max();// для дальшейшего создания бакетов
+            var min = unsorted.Min();
+            var bucketCount = (int)Math.Sqrt(unsorted.Length);// наиболее эффективное распределени
+            var buck = new List<int>[bucketCount];
+            var bucketSize = (int)Math.Ceiling((max + 1 - min) / (decimal)bucketCount);
+            for (int i = 0; i < bucketCount; i++)
+            {
+                buck[i] = new List<int>();
+                interationsArray++;
+            }
+            for (int i = 0; i < unsorted.Length; i++)
+            {// по бакетам
+                var number = unsorted[i];
+                var bucketIndex = (number - min) / bucketSize;
+                buck[bucketIndex].Add(number);
+                interationsArray++;
+            }
+
+            foreach (var bucket in buck)
+            {
+                interationsArray++;
+                bucket.Sort();
+            }
+            // соединение в исходный массив
+            return buck.SelectMany(x => x).ToArray();
+        }
+
+        public static void MeasureTimeIntegration(int[] array, Func<int[], int[]> searchProcedure, Series series)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+                searchProcedure(array);
+            watch.Stop();
+            series.Points.Add(new DataPoint(array.Length, interationsArray));
+            interationsArray = 0;
+        }
+
+    }
+}
---
+++ 2018/spring/AISD/Semestrovka1/BucketSortLinked.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace bucketlist
+{
+    public class BucketSortLinked
+    {// счетчик
+        static int interationsLinked = 0;
+
+        public static int[] BucketSortWithLinked(int[] unsorted)
+        {
+            var buck = new List<LinkedList<int>>();
+            var max = unsorted.Max();
+            var min = unsorted.Min();
+            var bucketCount = (int)Math.Sqrt(unsorted.Length);
+            var bucketSize = (int)Math.Ceiling((max + 1 - min) / (decimal)bucketCount);
+            for (int i = 0; i < bucketCount; i++)
+            {
+                buck.Add(new LinkedList<int>());
+                interationsLinked++;
+            }
+            for (int i = 0; i < unsorted.Length; i++)
+            {// загружаю по бакетам
+                interationsLinked++;
+                var number = unsorted[i];
+                var bucketIndex = (number - min) / bucketSize;
+                buck[bucketIndex].AddFirst(number);
+            }
+            foreach (var e in buck)
+            {
+                interationsLinked++;
+                SortLinked(e);
+            }
+            // соединение в исходный массив
+            return buck.SelectMany(x => x).ToArray();
+        }
+
+        private static void SortLinked(LinkedList<int> list)
+        {// Сортировка листа
+            var secondList = new LinkedList<int>();
+            while (list.Count != 0)
+            {
+                interationsLinked++;
+                var max = list.Max();
+                secondList.AddFirst(max);
+                list.Remove(max);
+            }
+            foreach (var e in secondList)
+            {
+                interationsLinked++;
+                list.AddLast(e);
+            }
+        }
+
+        public static void MeasureTimeIntegration(int[] array, Func<int[], int[]> searchProcedure, Series series)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+                searchProcedure(array);
+            watch.Stop();
+            series.Points.Add(new DataPoint(array.Length,interationsLinked));
+            interationsLinked = 0;
+        }
+    }
+}
